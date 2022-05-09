5208c5aded6af7e82aec6ffebb31894006aec22a
Ego <augurto@gmail.com>
2022-05-09 13:21:31 -0500

    agregando modal al index

--- index.php
+++ index.php
@@ -43,7 +43,34 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
 
     <?php include 'includes/parts/graficas.php'; ?>
     <!-- Fin de graficas -->
+    <!-- Boton agregar proyecto -->
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Guardar</button>
+      </div>
+    </div>
+  </div>
+</div>
+    <!-- Fin Boton agregar proyecto -->
+    
     <!-- Contenido de la tabla -->
     <?php include 'includes/parts/tabla-inicio.php'; ?>
     <!-- Fin del contenido de la tabla -->
