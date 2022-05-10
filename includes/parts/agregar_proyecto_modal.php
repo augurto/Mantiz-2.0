f5fe7b7d2c2cce0f9c41d63a43e943ed4e760dfe
Ego <augurto.17@gmail.com>
2022-05-10 14:02:39 -0500

    icon y lable dentro del modal

--- includes/parts/agregar_proyecto_modal.php
+++ includes/parts/agregar_proyecto_modal.php
@@ -26,9 +26,9 @@
                         <input type="text" class="form-control" id="nombre_proyecto" aria-describedby="emailHelp" placeholder="Enter email">
 
                         <div class="input-group mb-3">
-                          <label for=""></label>
+                          <label for="inputProyec">Nombre del Proyecto</label>
                           <div class="input-group-prepend">
-                            <span class="input-group-text">$</span>
+                            <span class="input-group-text"><i class="fa fa-archive" aria-hidden="true"></i></span>
                           </div>
                           <input type="text" class="form-control" placeholder="Nombre del Proyecto" >
                           <div class="input-group-append">
@@ -44,50 +44,7 @@
 
                         </form>
 
-                        <form class="row g-3">
-                          <div class="col-md-6">
-                            <label for="inputEmail4" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="inputEmail4">
-                          </div>
-                          <div class="col-md-6">
-                            <label for="inputPassword4" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="inputPassword4">
-                          </div>
-                          <div class="col-12">
-                            <label for="inputAddress" class="form-label">Address</label>
-                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                          </div>
-                          <div class="col-12">
-                            <label for="inputAddress2" class="form-label">Address 2</label>
-                            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                          </div>
-                          <div class="col-md-6">
-                            <label for="inputCity" class="form-label">City</label>
-                            <input type="text" class="form-control" id="inputCity">
-                          </div>
-                          <div class="col-md-4">
-                            <label for="inputState" class="form-label">State</label>
-                            <select id="inputState" class="form-select">
-                              <option selected>Choose...</option>
-                              <option>...</option>
-                            </select>
-                          </div>
-                          <div class="col-md-2">
-                            <label for="inputZip" class="form-label">Zip</label>
-                            <input type="text" class="form-control" id="inputZip">
-                          </div>
-                          <div class="col-12">
-                            <div class="form-check">
-                              <input class="form-check-input" type="checkbox" id="gridCheck">
-                              <label class="form-check-label" for="gridCheck">
-                                Check me out
-                              </label>
-                            </div>
-                          </div>
-                          <div class="col-12">
-                            <button type="submit" class="btn btn-primary">Sign in</button>
-                          </div>
-                        </form>
+                        
                       </div>
                     </div>
                   </div>
