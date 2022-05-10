73724f7d082a95992ee37bc7d9378eae69a67a71
Ego <augurto.17@gmail.com>
2022-05-10 15:44:09 -0500

    abora

--- includes/parts/agregar_proyecto_modal.php
+++ includes/parts/agregar_proyecto_modal.php
@@ -134,24 +134,7 @@
                           
                           <br>
                           <div class="input-group-prepend">
-                          <table class="table table-dark">
-                        <thead>
-                          ...
-                        </thead>
-                        <tbody>
-                          <tr class="table-active">
-                            ...
-                          </tr>
-                          <tr>
-                            ...
-                          </tr>
-                          <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2" class="table-active">Larry the Bird</td>
-                            <td>@twitter</td>
-                          </tr>
-                        </tbody>
-                      </table>
+                              <?php include 'tabla-inicio.php'; ?>
 
                           </div>
                           
