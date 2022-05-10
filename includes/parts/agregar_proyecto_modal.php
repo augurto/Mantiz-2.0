021d356f341ef14d67dc170c80ae05152799e0a9
Ego <augurto.17@gmail.com>
2022-05-10 15:32:05 -0500

    ingresando tabla de manera manual

--- includes/parts/agregar_proyecto_modal.php
+++ includes/parts/agregar_proyecto_modal.php
@@ -133,10 +133,27 @@
 
                         
                       </div>
-                    
+                      <table class="table table-dark">
+                        <thead>
+                          ...
+                        </thead>
+                        <tbody>
+                          <tr class="table-active">
+                            ...
+                          </tr>
+                          <tr>
+                            ...
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2" class="table-active">Larry the Bird</td>
+                            <td>@twitter</td>
+                          </tr>
+                        </tbody>
+                      </table>
                     </div>
                   </div>
-                  <?php include 'tabla-inicio.php'; ?>
+                  
                   </div>
 
                   
