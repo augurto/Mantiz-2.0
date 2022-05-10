cc32c407348945108b0e18ae7004a9d8a33f555b
Ego <augurto.17@gmail.com>
2022-05-10 15:41:40 -0500

    espacio encimade dfooter

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,10 @@
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 <footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
   <!-- Grid container -->
   <div class="container p-4"></div>
