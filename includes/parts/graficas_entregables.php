bc103efd57f692c6a5b0575923615e5085629fe4
Ego <augurto.17@gmail.com>
2022-05-11 12:25:34 -0500

    tps3

--- includes/parts/graficas_entregables.php
+++ includes/parts/graficas_entregables.php
@@ -92,7 +92,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Usarios activos en este proyecto</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tp3;?></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tps3;?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-users fa-2x text-gray-300"></i>
