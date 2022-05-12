76658ef92074022d19a1902244cef02dbd4d311b
Ego <augurto.17@gmail.com>
2022-05-12 04:25:24 -0500

    MODIFICACION EN PROYECTO JS

--- js/proyecto.js
+++ js/proyecto.js
@@ -3,16 +3,16 @@ function obtener_datos(id){
     var codigo = $("#codigo"+id).val();
     var estado = $("#estado"+id).val();
     var presupuesto = $("#presupuesto"+id).val();
+
+    var descripcion = $("#descripcion"+id).val();
     var cod = $("#cod"+id).val();
-    var mod_descripcion = $("#descripcion"+id).val();
     
 
     $("#mod_nombre").val(nombre);
     $("#mod_estado").val(estado);
     $("#mod_codigo").val(codigo);
     $("#mod_cod").val(cod);
-    
     $("#mod_presupuesto").val(presupuesto);
-    $("#mod_descripcion").val(mod_descripcion);
+    $("#mod_descripcion").val(descripcion);
     $("#mod_id").val(id);
 }
