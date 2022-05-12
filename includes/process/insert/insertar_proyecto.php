cbbd2fe49ad0532356bce201cce8d3fb9deacd34
Ego <augurto.17@gmail.com>
2022-05-12 01:36:43 -0500

    redireccionando el formulario

--- includes/process/insert/insertar_proyecto.php
+++ includes/process/insert/insertar_proyecto.php
@@ -10,6 +10,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 $nombre_proyecto=$_POST["nombre-proyecto"];
 $extension=$_POST["extension-codigo"];
 $codigo=$_POST["codigo"];
+$nuevo_codigo=$extension.'-'.$codigo;
 $presupuesto=$_POST["presupuesto"];
 $fecha=date("Y-m-d"); 
 $fecha_ini=$_POST["fecha_ini"];
@@ -23,7 +24,7 @@ echo "Connected successfully";
  
 $sql = "INSERT INTO proyecto (codigo, nombre_proyecto, presupuesto, estado,usuario_maker, fecha_agregado, fecha_ini, fecha_fin) 
 
-VALUES ('$codigo', '$nombre_proyecto', '$presupuesto', '2','Ego','$fecha','$fecha_ini','$fecha_fin')";
+VALUES ('$nuevo_codigo', '$nombre_proyecto', '$presupuesto', '2','Ego','$fecha','$fecha_ini','$fecha_fin')";
 if (mysqli_query($conn, $sql)) {
       echo "New record created successfully";
 } else {
