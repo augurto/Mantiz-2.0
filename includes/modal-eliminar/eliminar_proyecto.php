9d7d38722c97c3305f75b668b75e281b6d6fab0c
Ego <augurto.17@gmail.com>
2022-05-12 06:24:51 -0500

    ocultando codigo hidden

--- includes/modal-eliminar/eliminar_proyecto.php
+++ includes/modal-eliminar/eliminar_proyecto.php
@@ -21,11 +21,12 @@
                             </button>
                     </div>
                         <div class="modal-body">
-                            <input type="text" id="codigo" name="" ><br>
+                            <input type="hidden" id="codigo" name="" ><br>
                             <input type="text" id="apellido" name="">
                             <div class="alert alert-warning" role="alert">
                             <label for=""> Estas seguro que quieres eliminar el proyecto <?php ?> </label>
-                            <label for="" id="apellido"></label>
+                            
+                            <p id="apellido"></p>
                           </div>
                         </div>
                     <div class="modal-footer">
