7ca4fcd24964f09e97e4443bfe7f06ad471a1702
Ego <augurto.17@gmail.com>
2022-05-12 00:13:06 -0500

    prparando para el if

--- includes/parts/tabla_inventario_general.php
+++ includes/parts/tabla_inventario_general.php
@@ -18,12 +18,18 @@
                                 </tr>
                                 </thead>
                         <?php foreach ($link->query('SELECT * from obras') as $row){ // aca se hace la consulta e iterarla con each. ?> 
-                        <tr style="background-color: red !important;">
+                        <?php
+                        $nombre_obra=$row['nombre_obra'];
+                        $fecha_agregada=$row['fecha_agregada'];
+                        $estado=$row['estado'];
+
+                       ?>
+                        <tr style="background-color: blue !important;">
                             
-                            <td ><?php echo $row['nombre_obra'] ?></td>
-                            <td><?php echo $row['fecha_agregada'] ?></td>
-                            <td><?php echo $row['estado'] ?></td>
-                            <td>Accion Pendiente</td>
+                            <td><?php echo $nombre_obra ?></td>
+                            <td><?php echo $fecha_agregada ?></td>
+                            <td><?php echo $estado ?></td>
+                            <td>Boton Accion Pendiente</td>
                         </tr>
                         <?php
                             }
