8370b4816369dba02a6842e5b4555dd300a24eba
Ego <augurto.17@gmail.com>
2022-05-12 04:58:27 -0500

    agregando requerimientos del js

--- index.php
+++ index.php
@@ -34,6 +34,8 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
     <!-- fin datatable -->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Mantiz</title>
 </head>
 <body>
