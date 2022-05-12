7a16abfbafd5f756173223c2e0e69f8bd2e77fb8
Ego <augurto.17@gmail.com>
2022-05-12 05:58:25 -0500

    reduciendo codigo

--- includes/parts/tabla-inicio.php
+++ includes/parts/tabla-inicio.php
@@ -73,7 +73,7 @@
                             <td>
                          <!-- Button trigger modal -->
                                     <button type="button" id="btnmodal" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-nom="<?php echo $codigo; ?>" data-ape="mendez">
-                                      Launch demo modal
+                                      Editar
                                     </button>
 
                                     <script >
@@ -96,27 +96,7 @@
                                     </script>
 
 
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                      <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                          <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                              <span aria-hidden="true">&times;</span>
-                                            </button>
-                                          </div>
-                                          <div class="modal-body">
-                                          <label>nombre  : </label><input type="text" id="nombre" name="" ><br>
-                                          <label>apellido : </label><input type="text" id="apellido" name="">
-                                          </div>
-                                          <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                            <button type="button" class="btn btn-primary">Save changes</button>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
+                                    
 
 
                             </td>
