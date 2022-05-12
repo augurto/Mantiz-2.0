53089de50e18d02568bb6a55cdd438df6f328426
Ego <augurto.17@gmail.com>
2022-05-12 05:48:43 -0500

    haber haber

--- includes/parts/tabla-inicio.php
+++ includes/parts/tabla-inicio.php
@@ -71,34 +71,53 @@
                                 echo 'Activo';
                             }?></td>
                             <td>
-                            <!-- Button trigger modal -->
-                              <button type="button" id="btnmodal" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-cod="Ego">
-                                editt
-                              </button>
-                              <script>
-                                    $(document).on("click", "#btnmodal",function(){
-                                      var codigo =$(this).data('cod');
-                                      $("#codigo").val(codigo);
-                                    })
-                                  </script>
-                              <!-- Modal -->
-                              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                         <!-- Button trigger modal -->
+                                    <button type="button" id="btnmodal" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-nom="jose" data-ape="mendez">
+                                      Launch demo modal
+                                    </button>
+
+                                    <script >
+                                      $(document).on("click", "#btnmodal",function () {
+                                        
+                                              var nombre =$(this).data('nom');
+                                          var apellido =$(this).data('ape');
+
+                                        $("#nombre").val(nombre);
+                                        $("#apellido").val(apellido);
+
+
+                                        
+                                      })
+
+
+
+
+
+                                    </script>
+
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                      <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                              <span aria-hidden="true">&times;</span>
+                                            </button>
+                                          </div>
+                                          <div class="modal-body">
+                                          <label>nombre  : </label><input type="text" id="nombre" name="" ><br>
+                                          <label>apellido : </label><input type="text" id="apellido" name="">
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                          </div>
+                                        </div>
+                                      </div>
                                     </div>
-                                    <div class="modal-body">
-                                          <input type="text" id="codigo" name="">
-                                    </div>
-                                    <div class="modal-footer">
-                                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                      <button type="button" class="btn btn-primary">Save changes</button>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
+
 
                             </td>
                             <td>
