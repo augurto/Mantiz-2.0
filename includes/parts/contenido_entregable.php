7f77ca3b40a2fbb16df000528eaffce45610dec0
Ego <augurto.17@gmail.com>
2022-05-11 22:03:08 -0500

    cambiando 0 a activo

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -61,7 +61,12 @@
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary"><?php echo $nom." - ".$estado_entregable; ?></h6>
+                <!-- <h6 class="m-0 font-weight-bold text-primary"><?php echo $nom." - ".$estado_entregable; ?></h6> -->
+                <h6 class="m-0 font-weight-bold text-primary"><?php echo $nom." - "; ?><?php if ($estado_entregable==0) {
+                    # code...
+                    echo 'Activo';
+                } ?></h6>
+                
                  
                 </div>
                 <!-- Card Body -->
