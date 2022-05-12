cca84bff6b97fbf6687fd73c839fc117cf9c4ae0
Ego <augurto.17@gmail.com>
2022-05-12 04:29:29 -0500

    llevando todo a $id

--- includes/parts/tabla-inicio.php
+++ includes/parts/tabla-inicio.php
@@ -26,7 +26,7 @@
                         foreach ($link->query('SELECT * from proyecto order by codigo desc') as $row){ // aca se hace la consulta e iterarla con each. ?> 
                         <?php
                         
-                        $id_proyecto=$row['id'];
+                        $id=$row['id'];
                         $codigo=$row['codigo'];
                         $extencion=$row['extencion'];
                         $nombre_proyecto=$row['nombre_proyecto'];
@@ -75,7 +75,7 @@
                             <a href="../../includes/process/eliminar/eliminar_proyecto.php?id_p=<?php echo $id_proyecto; ?>" class="btn btn-danger"  title='Borrar proyecto desde a'><i class="fa fa-trash"></i></a>
                             <!-- <a href="#" class="btn btn-danger" data-toggle="modal" data-target="#eliminar-proyecto"  title='Borrar proyecto'><i class="fa fa-trash"></i></a> -->
                                <span class="pull-right">
-                                <a href="#" class='btn btn-primary' data-toggle="modal" data-target="#editProyecto" title='Editar proyecto'  onclick="obtener_datos(<?php echo $id_proyecto;?>);"><i class="fa fa-edit"></i></a>
+                                <a href="#" class='btn btn-primary' data-toggle="modal" data-target="#editProyecto" title='Editar proyecto'  onclick="obtener_datos(<?php echo $id;?>);"><i class="fa fa-edit"></i></a>
                                 <!--
                                 <a href="#" class="btn btn-danger" data-target="#eliminar-proyecto" title='Borrar proyecto'><i class="fa fa-trash"></i></a>
                               
