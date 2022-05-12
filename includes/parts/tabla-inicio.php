17c046d3b82cba020189584d59ac31f87aafb247
Ego <augurto.17@gmail.com>
2022-05-12 04:04:22 -0500

    url mejorada

--- includes/parts/tabla-inicio.php
+++ includes/parts/tabla-inicio.php
@@ -71,7 +71,8 @@
                             <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#eliminar-proyecto">
                             <i class="fa fa-trash"></i>
                             </button>
-                            <a href="../process/eliminar/eliminar_proyecto.php?id_p=<?php echo $id_proyecto; ?>" class="btn btn-danger"  title='Borrar proyecto 1'><i class="fa fa-trash"></i></a>
+                            
+                            <a href="../../includes/process/eliminar/eliminar_proyecto.php echo $id_proyecto; ?>" class="btn btn-danger"  title='Borrar proyecto desde a'><i class="fa fa-trash"></i></a>
                             <a href="#" class="btn btn-danger" data-toggle="modal" data-target="#eliminar-proyecto"  title='Borrar proyecto'><i class="fa fa-trash"></i></a>
                               <!-- <span class="pull-right">
                                 <a href="#" class='btn btn-primary' data-toggle="modal" data-target="#editProyecto" title='Editar proyecto'  onclick="obtener_datos(<?php echo $id;?>);"><i class="fa fa-edit"></i></a>
