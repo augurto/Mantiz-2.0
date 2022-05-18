9176fb4061daedcb836ce37f631c7154a58139aa
Ego <augurto.17@gmail.com>
2022-05-18 01:50:24 -0500

    probando mostrar contenido

--- includes/modal/modal-subir-archivos.php
+++ includes/modal/modal-subir-archivos.php
@@ -1,21 +1,48 @@
 
+<script type="text/javascript">
+    function showContent() {
+        element = document.getElementById("content");
+        check = document.getElementById("check");
+        if (check.checked) {
+            element.style.display='block';
+        }
+        else {
+            element.style.display='none';
+        }
+    }
+</script>
+
 <!-- Modal -->
-<div class="modal fade" id="modal-subir-archivos" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+<form action="">
+
+<div class="modal fade" id="modal-subir-archivos" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Subir Archivos</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <h5 class="modal-title" id="staticBackdropLabel">Agregar entregable</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+       <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Descripcion</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      </div>
+      <b>Mostrar contenido?</b>
+      <input type="checkbox" name="check" id="check" value="1" onchange="javascript:showContent()" />
+        <div id="content" style="display: none;">
+          contenido del div escondido<br/>
+          contenido del div escondido<br/>
+          contenido del div escondido<br/>
+        </div>
+
+
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Subir Archivo</button>
       </div>
     </div>
   </div>
 </div>
+
+</form>
