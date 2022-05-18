af90bc058ce5c0a5aa07125b59bf3e8b8895d9c1
Ego <augurto.17@gmail.com>
2022-05-18 01:58:36 -0500

    cambio a otro modal

--- includes/modal/modal-subir-archivos.php
+++ includes/modal/modal-subir-archivos.php
@@ -12,19 +12,20 @@
     }
 </script>
 
+
 <!-- Modal -->
-<div class="modal fade" id="modal-subir-archivos" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-  <form action="">
+<form action="../../includes/process/insert/insertar_entregable.php" >
+<div class="modal fade" id="modal-subir-archivos" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <h5 class="modal-title" id="exampleModalLongTitle">Agregar Entregables</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-      
       <div class="modal-body">
-        
-      
+          
       <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon1">Descripcion</span>
         <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
@@ -37,17 +38,13 @@
           contenido del div escondido<br/>
         </div>
 
-
       </div>
-
-      
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary">Agregar Entregable</button>
       </div>
     </div>
-    </form>
   </div>
 </div>
 
-
+</form>
