55c8aa6f4875faba43edcb36b3e98fff41f65041
Ego <augurto.17@gmail.com>
2022-05-18 01:28:56 -0500

    quitna metodo post

--- includes/modal/modal_entregable.php
+++ includes/modal/modal_entregable.php
@@ -1,6 +1,6 @@
 
 <!-- Modal -->
-<form action="../../includes/process/insert/insertar_entregable.php" method="post">
+<form action="../../includes/process/insert/insertar_entregable.php" >
 <div class="modal fade" id="modal-entregable" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
