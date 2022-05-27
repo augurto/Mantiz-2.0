7d0026fea8357dd2477e3205bb8737e3cf8ebe19
Ego <augurto.17@gmail.com>
2022-05-27 04:30:12 -0500

    progress bar

--- includes/parts/graficas_entregables.php
+++ includes/parts/graficas_entregables.php
@@ -103,3 +103,6 @@
             </div>
           </div>
 </div>
+<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+</div>
