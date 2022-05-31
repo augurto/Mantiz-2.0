90e27ae21660342a60521f7b273ca1fcb3ea2397
Ego <augurto.17@gmail.com>
2022-05-31 04:38:58 -0500

    submit p babosoo

--- includes/modal-editar/editar_proyecto.php
+++ includes/modal-editar/editar_proyecto.php
@@ -80,7 +80,7 @@
                         </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                        <button type="button" class="btn btn-primary">Actualizar</button>
+                        <button type="submit" class="btn btn-primary">Actualizar</button>
                     </div>
                     </form>
             </div>
