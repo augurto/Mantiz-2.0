840ebfbea14757e0403f574b9ae5054c56951025
Ego <augurto.17@gmail.com>
2022-06-02 02:52:48 -0500

    login en index

--- login/index.php
+++ login/index.php
@@ -1,4 +1,92 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: ./index.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: ./index.php");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
 
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -36,15 +124,21 @@
               <h3>Iniciar Sesion en  <strong>Mantiz</strong></h3>
               <p class="mb-4">Floro para nuestro sensual software.</p>
             </div>
-            <form action="#" method="post">
+            <?php 
+              if(!empty($login_err)){
+                  echo '<div class="alert alert-danger">' . $login_err . '</div>';
+              }        
+            ?>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
               <div class="form-group first">
                 <label for="username">Usuario</label>
-                <input type="text" class="form-control" id="username">
-
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
               </div>
               <div class="form-group last mb-4">
                 <label for="password">Contraseña</label>
-                <input type="password" class="form-control" id="password">
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
                 
               </div>
               
@@ -61,7 +155,8 @@
 
               <span class="ml-auto"><a href="#" class="forgot-pass">Has olvidado tu contraseña, mejora olvidala a ella </a></span> 
               <br>
-              <input type="submit" value="Ingresar" class="btn text-white btn-block btn-primary">
+              <input type="submit" class="btn btn-primary" value="Login">
+              <p>No tienes cuenta? <a href="registro.php">Registrate</a>.</p>
 
              <!--  inicio de sesion con redes sociales
               <span class="d-block text-left my-4 text-muted"> or sign in with</span>
