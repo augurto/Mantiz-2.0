e88ce820a4a3ce527e03f0b1d706b2522b2630d7
Ego <augurto.17@gmail.com>
2022-06-02 04:28:03 -0500

    yipo usuario mostrar

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
   <a class="navbar-brand" href="#">Logo Mantiz</a>
-  <a class="navbar-brand" href="#">Bienvenido <?php echo $usuario.' '.$id_usuario;?></a>
+  <a class="navbar-brand" href="#">Bienvenido <?php echo $usuario.' '.$tipo_usuario;?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
