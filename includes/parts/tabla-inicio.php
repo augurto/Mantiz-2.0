c2a7ecfb50aae03e00c73f784adb13134d987a97
Ego <augurto.17@gmail.com>
2022-06-07 07:58:49 -0500

    quitando la sonsera

--- includes/parts/tabla-inicio.php
+++ includes/parts/tabla-inicio.php
@@ -86,7 +86,7 @@
                                     </button>
 
                                     
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button>
+                                   
 
                             </td>
                             <?php }
