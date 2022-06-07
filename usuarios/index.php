bbe1706d1b387ec8b2ef46a373afafdeae1216a3
Ego <augurto.17@gmail.com>
2022-06-07 08:40:50 -0500

    faltaba mandar atras un css

--- usuarios/index.php
+++ usuarios/index.php
@@ -25,7 +25,7 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- incluyendo bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.min.css">
     <script src="../js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../css/style.css">
 
