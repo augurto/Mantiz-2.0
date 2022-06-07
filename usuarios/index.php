82dcaec06290664412511389d36ed2cfc1044ec4
Ego <augurto.17@gmail.com>
2022-06-07 08:29:05 -0500

    url de boostrap

--- usuarios/index.php
+++ usuarios/index.php
@@ -22,8 +22,13 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
 <html>
 <head>
   <title>Usuarios</title>
+  <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.min.css">
+    <script src="../js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+
 </head>
  
 <body>
