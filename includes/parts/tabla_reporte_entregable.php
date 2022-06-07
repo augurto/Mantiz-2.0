124be1012ef299986dfb5484101d0749f4629d24
Ego <augurto.17@gmail.com>
2022-06-07 00:29:48 -0500

    seleccionando cada dato

--- includes/parts/tabla_reporte_entregable.php
+++ includes/parts/tabla_reporte_entregable.php
@@ -29,7 +29,7 @@
                         $count=1;
                         /* foreach ($link->query('SELECT * from entregables e inner join proyecto p on e.codigo_proyecto = p.codigo inner join archivos a on e.codigo_proyecto = a.codigo_proyecto order by a.codigo_proyecto desc') as $row){  ?>  */
                         
-                        foreach ($link->query('SELECT * from archivos a /* inner join entregables e on a.codigo_proyecto=e.codigo_proyecto inner join proyecto p on a.codigo_proyecto = p.codigo */') as $row){ // aca se hace la consulta e iterarla con each. ?> 
+                        foreach ($link->query('SELECT a.codigo_proyecto,a.documento, a.id_seg , a.descripcion,a.estado_seguimiento   from archivos a /* inner join entregables e on a.codigo_proyecto=e.codigo_proyecto inner join proyecto p on a.codigo_proyecto = p.codigo */') as $row){ // aca se hace la consulta e iterarla con each. ?> 
 
                         <?php
                         $a_codigo_proyecto=$row['a.codigo_proyecto'];
