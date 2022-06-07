58737e040a8569560a627b03135dd0006bd7b41f
Ego <augurto.17@gmail.com>
2022-06-07 08:15:03 -0500

    de nuevio

--- usuarios/index.php
+++ usuarios/index.php
@@ -1,14 +1,70 @@
+<?php
+
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../login/");
+    exit;
+}
+
+require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
+require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
+$sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where estado='terminado'");
+        $rwt=mysqli_fetch_array($sald);
+        $saldo=$rwt['saldo'];
+        $usuario=$_SESSION["username"];
+        $id_usuario=$_SESSION["id"];
+
+/* requiere para la extraer la informacion */
+        $id_p=$_GET['id_p'];
+        $sql=mysqli_query($con,"SELECT * FROM proyecto WHERE codigo='".$id_p."'");
+         $rws=mysqli_fetch_array($sql);
+         $nombre=$rws["nombre_proyecto"];
+?>
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-  <title>Formulario Dinamico</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- incluyendo bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- inicio datatables -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!-- CSS personalizado --> 
+    <link rel="stylesheet" href="main.css">  
+      
+    <!--datables CSS básico-->
+    <link rel="stylesheet" type="text/css" href="datatables/datatables.min.css"/>
+    <!--datables estilo bootstrap 4 CSS-->  
+    <link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
+           
+    <!--font awesome con CDN-->  
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">  
+
+    <!-- fin datatable -->
+    <title>Mantiz-Entregables</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
- 
 <body>
+<!-- datos de sesion -->
+<input type="hidden" value="<?php echo $usuario;?>">
+  <input type="hidden" value="<?php echo $id_usuario;?>">
+  <!-- fin de datos sesion -->
 
+    <?php include 'includes/header.php';?>
+    <div style="height:50px"></div>
+    <!--   inicio contenido  -->  
 
+    
 <div class="container mt-5" style="background-color: #f9f9f9;">
   <br>
   <h1 class="text-center">
@@ -59,8 +115,34 @@
 </form>
 </div>
  
- 
-  <script type="text/javascript">
+    <!-- fin contenido -->
+    <?php  include 'includes/footer.php'?>
+
+
+    <!-- Inicio de Script para datatables -->
+
+      <!-- jQuery, Popper.js, Bootstrap JS -->
+      <script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+      
+    <!-- datatables JS -->
+    <script type="text/javascript" src="datatables/datatables.min.js"></script>    
+     
+    <!-- para usar botones en datatables JS -->  
+    <script src="datatables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>  
+    <script src="datatables/JSZip-2.5.0/jszip.min.js"></script>    
+    <script src="datatables/pdfmake-0.1.36/pdfmake.min.js"></script>    
+    <script src="datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
+    <script src="datatables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
+     
+    <!-- código JS propìo-->    
+    <script type="text/javascript" src="main.js"></script>   
+
+    <!-- Fin de Script para datatables -->
+
+    <!-- script para inpuyt dinamico -->
+    <script type="text/javascript">
     $(function () { 
       var i = 1;
       $('.add-btn').click(function (e) {
