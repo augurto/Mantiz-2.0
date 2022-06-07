7d3a837f8ad5851ec5c817932d2c62c2c9e10e08
Ego <augurto.17@gmail.com>
2022-06-07 04:16:45 -0500

    no se ahhh

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -61,9 +61,8 @@
                 <div class="col-xl-4 col-lg-5">
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <!-- <h6 class="m-0 font-weight-bold text-primary"><?php echo $nom." - ".$estado_entregable; ?></h6> -->
                 <div class="input-group input-group-sm mb-3">
+                <!-- <h6 class="m-0 font-weight-bold text-primary"><?php echo $nom." - ".$estado_entregable; ?></h6> -->
                 <h6 class="m-0 font-weight-bold text-primary"><?php echo $nom." - "; ?><?php if ($estado_entregable==0) {
                     # code...
                     echo 'Pendiente';
@@ -73,6 +72,7 @@
                 } else{
                     echo 'Observado';
                 }?></h6>
+                <div class="input-group input-group-sm mb-3">
                 <form action="">
                 
                 <select class="form-select form-select-sm" aria-label=".form-select-sm example">
