d7a3af078b286ed131b0fccbc0cc1048dc3527a7
Ego <augurto.17@gmail.com>
2022-06-07 10:01:28 -0500

    colores

--- includes/parts/tabla_reporte_entregable.php
+++ includes/parts/tabla_reporte_entregable.php
@@ -47,13 +47,13 @@
                         $estado=$row['estado'];
                          if ($fecha_actual>$fecha_v) { ?>
                              
-                             <tr style="background-color: #F0FFFF !important;">
+                             <tr style="background-color: #005BF9 !important;">
 
                              <?php  }elseif ($fecha_actual==$fecha_v) { ?>
-                             <tr style="background-color: #F0FFF0 !important;">
+                             <tr style="background-color: #FFE97F !important;">
 
                              <?php  } else{?>
-                             <tr style="background-color: #FDF5E6 !important;">
+                             <tr style="background-color: #FC1900 !important;">
                              <?php }?>   
                              <td><?php echo $count++; ?></td>
                              
