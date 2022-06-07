c56a0e36ed96a5b1388530efd82036c4561248e7
Ego <augurto.17@gmail.com>
2022-06-07 04:39:35 -0500

    nmo estaba igaulando al id

--- includes/process/actualizar/actualizar_estado_entregable.php
+++ includes/process/actualizar/actualizar_estado_entregable.php
@@ -6,7 +6,7 @@ $password = "Mantizb*#17";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
-$id=$_GET["codigo"];
+$id=$_GET["id"];
 $id_p=$_GET["id_p"];
 $estado_entregable=$_GET["estado_entregable1"];
 
@@ -21,6 +21,7 @@ if (!$conn) {
 echo "Connected successfully";
  
 $sql = "UPDATE  archivos set  a_estado_seguimiento = '".$estado_entregable."' where a_id = '".$id."'";
+/* $sql = "UPDATE  users set  tipo_user = '1' where id = '".$id_users."'"; */
 if (mysqli_query($conn, $sql)) {
     echo"<script language ='JavaScript'>";
       echo "location='../../../entregables.php?id_p=$id_p'";
