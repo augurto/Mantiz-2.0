f3410bda4228e3005e98a5f3d2175e0676a5cfc2
Ego <augurto.17@gmail.com>
2022-06-07 02:32:24 -0500

    faltaba la s

--- includes/parts/graficas.php
+++ includes/parts/graficas.php
@@ -8,7 +8,7 @@
       $rwe=mysqli_fetch_array($te);
       $tes=$rwe["te"];
 
-      $ti=mysqli_query($con,"SELECT count(*) ti FROM user /* where rol='investigador' */");
+      $ti=mysqli_query($con,"SELECT count(*) ti FROM users /* where rol='investigador' */");
       $rwi=mysqli_fetch_array($ti);
       $tin=$rwi["ti"];
 
