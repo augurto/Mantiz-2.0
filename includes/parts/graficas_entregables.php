6fb7ce5af54b12575eb227f284f97246bb980401
Ego <augurto.17@gmail.com>
2022-06-07 04:55:28 -0500

    copaindo la parte interna

--- includes/parts/graficas_entregables.php
+++ includes/parts/graficas_entregables.php
@@ -129,23 +129,24 @@
                     </tr>
                     </thead>
                 <?php foreach ($link->query('SELECT * from entregables where codigo_proyecto="'.$cod1.'"') as $row){ // aca puedes hacer la consulta e iterarla con each. ?> 
+                  <?php 
+                      $bar=mysqli_query($con,"SELECT COUNT(id_seg) as tp2 FROM archivos where id_seg= '".$row['id']."'");
+                      $rwp21=mysqli_fetch_array($bar);
+                      $tps21=$rwp21["tp2"];
+                      $porcentaje_entregable=(100/$tps21);
+                      $bar2=mysqli_query($con,"SELECT estado_seguimiento as tp22 FROM archivos where id_seg= '".$row['id']."' AND estado_seguimiento=1");
+                      $rwp21=mysqli_fetch_array($bar2);
+                      $tps211=$rwp211["tp22"];
+                     
+                      
+                      ?>
                 <tr>
                   
                     <td><?php echo $row['nombre'] ?></td>
                     <td>
 
                     <div class="progress">
-                      <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: <?php echo $porcentaje_entregable;?>" aria-valuenow="<?php echo $porcentaje_entregable;?>" aria-valuemin="0" aria-valuemax="100"><?php echo $porcentaje_entregable ;?>/<?php 
-                      $bar=mysqli_query($con,"SELECT COUNT(id_seg) as tp2 FROM archivos where id_seg= '".$row['id']."'");
-                      $rwp21=mysqli_fetch_array($bar);
-                      $tps21=$rwp21["tp2"];
-                      $porcentaje_entregable=(100/$tps21).'%';
-                      $bar2=mysqli_query($con,"SELECT estado_seguimiento as tp22 FROM archivos where id_seg= '".$row['id']."' AND estado_seguimiento=1");
-                      $rwp21=mysqli_fetch_array($bar2);
-                      $tps211=$rwp211["tp22"];
-                     
-                      
-                      ?><?php echo $porcentaje_entregable.''.$tps211; ?> </div>
+                      <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: <?php echo $porcentaje_entregable;?>" aria-valuenow="<?php echo $porcentaje_entregable;?>" aria-valuemin="0" aria-valuemax="100"><?php echo $porcentaje_entregable ;?><?php echo $porcentaje_entregable; ?>% </div>
                     </div>
 
                     </td>
