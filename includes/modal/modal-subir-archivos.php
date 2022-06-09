287aa0e26c718906f53179c14161622564abf8ce
Ego <augurto.17@gmail.com>
2022-06-09 03:19:26 -0500

    mostrando nombre de proyecto

--- includes/modal/modal-subir-archivos.php
+++ includes/modal/modal-subir-archivos.php
@@ -24,9 +24,21 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
+     
+                        <?php 
+
+                        $sss2=mysqli_query($con,"SELECT * FROM proyecto where codigo=$id_p");
+                                while($f2=mysqli_fetch_assoc($sss2)){    
+
+                                  $nombre_proyecto=$f2['nombre_proyecto'];
+
+                        }
+                        
+                        ?>
       <div class="modal-body">
       <div class="input-group mb-3">
       <input type="hidden" value="<?php echo $_GET["id_p"]; ?>" id="codigo" name="codigo" >
+      <input type="text" value="<?php echo $nombre_proyecto; ?>" id="codigop" name="codigop" >
       <input type="hidden" value="<?php echo $usuario; ?>" id="usuario" name="usuario" >
         <input type="file" class="form-control"  id="documento" name="documento" aria-label="Username" aria-describedby="basic-addon1">
       </div>  
