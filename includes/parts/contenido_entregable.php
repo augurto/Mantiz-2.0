662cff6c61395e72e796bafce604ab5ba6a258b5
Ego <augurto.17@gmail.com>
2022-06-11 01:23:31 -0500

    cardf7

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -71,7 +71,7 @@
                 <form action="../../includes/process/actualizar/actualizar_estado_entregable.php">
                 <input type="hidden" value="<?php echo $id; ?>" id="id" name="id" >
                 <input type="hidden" value="<?php echo $id_p; ?>" id="id_p" name="id_p" >
-                <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id="estado_entregable1" name="estado_entregable1">
+                <select class="form-select" aria-label="Default select example" id="estado_entregable1" name="estado_entregable1">
                   <option selected>Cambiar Estado</option>
                   <option value="0">Pendiente</option>
                   <option value="1">Aprobado</option>
@@ -87,7 +87,7 @@
                 <li class="list-group-item">
                 <p>URL: </p> <a href="<?php echo  $link  ; ?>/<?php echo  $link; ?>" download="<?php echo  $link  ; ?>/<?php echo  $link; ?>" target="_blank"><i class="fa fa-cloud-download" ></i>  <?php echo $link; ?></a>
                 <?php echo $descripcion; ?>
-                <?php echo "Usuario : " .$usuario_seguimiento; ?>
+                <?php echo "Usuario : " .$usuario_seguimiento.'  '; ?>
                 <a href="#"  data-toggle="modal" data-target="#comments" onclick="comments(<?php echo $id_ent; ?>, <?php echo $id; ?>);">
                 <?php echo $ts; ?> <i class="fas fa-comments fa-fw"></i>
                     <!-- Counter - Messages -->
