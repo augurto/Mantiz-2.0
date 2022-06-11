f2b45cc5aaea073c6bf2c56ebc147ecc64859496
Ego <augurto.17@gmail.com>
2022-06-11 06:27:03 -0500

    vxcvcxv

--- includes/parts/graficas_entregables.php
+++ includes/parts/graficas_entregables.php
@@ -39,7 +39,7 @@
                 <div class="row">
                         <div class="col-lg-12">
 
-                                <!-- Earnings (Monthly) Card Example -->
+                            <!-- 
                                 <div class="col-xl-3 col-md-6 mb-4">
                                   <div class="card border-left-primary shadow h-100 py-2">
                                     <div class="card-body">
@@ -49,14 +49,14 @@
                                           <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tps ;?></div>
                                         </div>
                                         <div class="col-auto">
-                                          <!-- <i class="fas fa-dollar-sign  fa-2x text-gray-300"></i> -->
+                                        
                                         </div>
                                       </div>
                                     </div>
                                   </div>
                                 </div>
 
-                                <!-- Earnings (Monthly) Card Example -->
+                               
                                 <div class="col-xl-3 col-md-6 mb-4">
                                   <div class="card border-left-success shadow h-100 py-2">
                                     <div class="card-body">
@@ -66,14 +66,14 @@
                                           <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tps2;?></div>
                                         </div>
                                         <div class="col-auto">
-                                          <!-- <i class="fas fa-clipboard-list fa-2x text-gray-300"></i> -->
+                                         
                                         </div>
                                       </div>
                                     </div>
                                   </div>
                                 </div>
 
-                                <!-- Earnings (Monthly) Card Example -->
+                               
                                 <div class="col-xl-3 col-md-6 mb-4">
                                   <div class="card border-left-info shadow h-100 py-2">
                                     <div class="card-body">
@@ -90,14 +90,14 @@
                                           </div>
                                         </div>
                                         <div class="col-auto">
-                                          <!-- <i class="fas fa-calendar fa-2x text-gray-300"></i> -->
+                                       
                                         </div>
                                       </div>
                                     </div>
                                   </div>
                                 </div>
 
-                                <!-- Pending Requests Card Example -->
+                              
                                 <div class="col-xl-3 col-md-6 mb-4">
                                   <div class="card border-left-warning shadow h-100 py-2">
                                     <div class="card-body">
@@ -117,7 +117,7 @@
                                     </div>
                                   </div>
                                 </div>
-                              </div>
+                              </div> -->
 
                               <?php
                                     
