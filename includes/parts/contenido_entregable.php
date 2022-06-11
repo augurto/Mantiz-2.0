af42675122ee0515e14ace74051e9b5ddaa38910
Ego <augurto.17@gmail.com>
2022-06-11 01:58:36 -0500

    nec

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -25,33 +25,8 @@
             $estado=$rwdt['estado'];
 
     ?>      
-              <div class="col-3">
-                  <ol class="list-group list-group-numbered">
-                      <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <div class="ms-2 me-auto">
-                          <div class="fw-bold">Subheading</div>
-                          Content for list item
-                        </div>
-                        <span class="badge bg-primary rounded-pill">14</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <div class="ms-2 me-auto">
-                          <div class="fw-bold">Subheading</div>
-                          Content for list item
-                        </div>
-                        <span class="badge bg-primary rounded-pill">14</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <div class="ms-2 me-auto">
-                          <div class="fw-bold">Subheading</div>
-                          Content for list item
-                        </div>
-                        <span class="badge bg-primary rounded-pill">14</span>
-                      </li>
-                    </ol>
-              </div>
-              </div>
-              </div>
+           
+            
             <div class="card text-bg-primary mb-3" style="width: 18rem;">
               <ul class="list-group list-group-flush">
                 <li class="list-group-item"><?php echo $nom." - "; ?><?php if ($estado_entregable==0) {
