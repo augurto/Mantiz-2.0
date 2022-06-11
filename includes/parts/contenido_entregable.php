abfa71e67242b363a95dd095814e782cc020dfb2
Ego <augurto.17@gmail.com>
2022-06-11 04:27:43 -0500

    terminado el card

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -93,8 +93,8 @@
                 <div class="input-group input-group-sm mb-3" >
                 <div class="input-group" style="padding: 0px 20px !important;">
              
-                <select class="form-select .text-primary" style="border-color: #0275d8 !important; background-color: 
-                <?php if ($estado_entregable==0) {echo '#FFF0F5';}elseif ($estado_entregable==1) {echo '#F0FFF0'; }else {echo '#FFF0F5';}?> !important;"  aria-label="Example select with button addon" id="estado_entregable1" name="estado_entregable1">
+                <select class="form-select .text-primary" style="border-color: #0275d8 !important;color: #0275d8 !important; background-color: 
+                <?php if ($estado_entregable==0) {echo '#FCF3CF';}elseif ($estado_entregable==1) {echo '#F0FFF0'; }else {echo '#FFF0F5';}?> !important;"  aria-label="Example select with button addon" id="estado_entregable1" name="estado_entregable1">
                 <option selected>Selecciona Estado</option>
                   <option value="0">Pendiente</option>
                   <option value="1">Aprobado</option>
