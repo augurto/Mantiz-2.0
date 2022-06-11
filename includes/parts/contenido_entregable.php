6a01bbbdcfebc50f4f6b43e35a4c34f717687947
Ego <augurto.17@gmail.com>
2022-06-11 01:38:18 -0500

    ol

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -25,7 +25,29 @@
             $estado=$rwdt['estado'];
 
     ?>      
-           
+           <ol class="list-group list-group-numbered">
+              <li class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="ms-2 me-auto">
+                  <div class="fw-bold">Subheading</div>
+                  Content for list item
+                </div>
+                <span class="badge bg-primary rounded-pill">14</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="ms-2 me-auto">
+                  <div class="fw-bold">Subheading</div>
+                  Content for list item
+                </div>
+                <span class="badge bg-primary rounded-pill">14</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="ms-2 me-auto">
+                  <div class="fw-bold">Subheading</div>
+                  Content for list item
+                </div>
+                <span class="badge bg-primary rounded-pill">14</span>
+              </li>
+            </ol>
             
             <div class="card text-bg-primary mb-3" style="width: 18rem;">
               <ul class="list-group list-group-flush">
