58fad96ac55a754a4e6325052f7a739e3b61f7e5
Ego <augurto.17@gmail.com>
2022-06-11 03:10:24 -0500

    qsalse

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -85,7 +85,15 @@
 </button>
                 </div>
                 
-                
+                <div class="input-group">
+                <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                  <option selected>Choose...</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+                <button class="btn btn-outline-secondary" type="button">Button</button>
+              </div>
               
                 </form>
                 <?php } ?>
