e9764cf103c6dbe4324d56fdc1e2485c28601b70
Ego <augurto.17@gmail.com>
2022-06-13 04:18:03 -0500

    confimrmando contenido

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -66,11 +66,11 @@
                                       <div class="modal-dialog">
                                         <div class="modal-content">
                                           <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <h5 class="modal-title" id="exampleModalLabel">Eliminar archivo</h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                           </div>
                                           <div class="modal-body">
-                                            ...
+                                          Seguro que quieres eliminar el archivo <?php echo $documento;?> del entregable <?php echo $nom;?>
                                           </div>
                                           <div class="modal-footer">
                                             <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Cancelar</button>
