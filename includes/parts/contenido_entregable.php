0669955c1314f00a9f5f3293f2a03b11b7b14576
Ego <augurto.17@gmail.com>
2022-06-13 03:45:51 -0500

    asczxc

--- includes/parts/contenido_entregable.php
+++ includes/parts/contenido_entregable.php
@@ -91,8 +91,9 @@
                                       <div class="input-group input-group-sm mb-3" >
                                           <div class="input-group" style="padding: 0px 20px !important;">
                                   
-                                                <select value="<?php echo $estado_entregable;?>"  class="form-select .text-primary"  aria-label="Example select with button addon" id="estado_entregable1" name="estado_entregable1" >
-                                                <option selected></option>
+                                                <select class="form-select .text-primary" style="border-color: #0275d8 !important;color: #0275d8 !important; background-color: 
+                                                <?php if ($estado_entregable==0) {echo '#FCF3CF';}elseif ($estado_entregable==1) {echo '#F0FFF0'; }else {echo '#FFF0F5';}?> !important;"  aria-label="Example select with button addon" id="estado_entregable1" name="estado_entregable1" value="<?php echo $estado_entregable;?>">
+                                                <!-- <option selected></option> -->
                                                   <option value="0">Pendiente</option>
                                                   <option value="1">Aprobado</option>
                                                   <option value="2">Observado</option>
