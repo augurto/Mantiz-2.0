e6c314ea70275de2d2d288a9db89898bcc722a65
Ego <augurto.17@gmail.com>
2022-06-13 07:46:55 -0500

    dando forma

--- includes/parts/modal_dentro_modal_tipo.php
+++ includes/parts/modal_dentro_modal_tipo.php
@@ -2,11 +2,31 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>
+        <h5 class="modal-title" id="exampleModalToggleLabel">Agregar Sub tipo Proyecto</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        Show a second modal and hide this one with the button below.
+      <form action="../../includes/process/insert/insertar_tipo_codigo_proyecto.php">
+                        
+                        <div class="modal-body">
+                        <label for="inputProyec">Nombre del Sub Tipo de Proyecto</label>
+                        <div class="input-group mb-3">
+                          
+                          <br>
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-sitemap" aria-hidden="true"></i></span>
+                          </div>
+                          <input type="text" class="form-control" placeholder="Nombre del Proyecto" id="nombre-tipo" name="nombre-tipo" >
+                          
+                        </div>
+
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                          <button type="submit" class="btn btn-primary">Agregar</button>
+                        </div>
+                        
+                        </form>
       </div>
       <div class="modal-footer">
         <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
