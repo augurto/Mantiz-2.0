c2022c3c1f5bc07aa615474649ca6c64878e6b13
Ego <augurto.17@gmail.com>
2022-06-13 05:39:43 -0500

    ojala

--- includes/parts/agregar_sub_tipo_proyecto.php
+++ includes/parts/agregar_sub_tipo_proyecto.php
@@ -66,41 +66,38 @@
                       
                     </div>
                   </div>
-                  <div class="modal fade" id="compromisos" tabindex="-1">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Compromisos</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <table id="compromiso_ejecutivo" class="display nowrap table-responsive">
-                        <thead>
-                          <tr>
-                              <th>No hay columnas</th>
-                              <th>No hay columnas</th>
-                              <th>No hay columnas</th>
-                              <th>No hay columnas</th>
-                              <th>No hay columnas</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
-                </div>
-                <script type="text/javascript">
-                    $(document).ready(function () {
-                        $('#compromisos > .modal-body').css({width:'auto',height:'auto', 'max-height':'100%'});
-                        $("#compromiso_ejecutivo").dataTable();
-                    });
-                </script>
-            </div>
-        </div>
-    </div>
-    
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#compromisos">
- Mostrar
-</button>
+
+
+                  <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                              <div class="modal-header">
+                                  <h5 class="modal-title" id="exampleModalLabel">Compromisos</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                  </button>
+                              </div>
+                              <div class="modal-body">
+                                  <table id="compromiso_ejecutivo" class="display nowrap table-responsive">
+                                      <thead>
+                                        <tr>
+                                            <th>No hay columnas</th>
+                                            <th>No hay columnas</th>
+                                            <th>No hay columnas</th>
+                                            <th>No hay columnas</th>
+                                            <th>No hay columnas</th>
+                                        </tr>
+                                      </thead>
+                                      <tbody>
+                                      
+                                      </tbody>
+                                  </table>
+                              </div>
+                              <script type="text/javascript">
+                                  $(document).ready(function () {
+                                      $('#compromisos > .modal-body').css({width:'auto',height:'auto', 'max-height':'100%'});
+                                      $("#compromiso_ejecutivo").dataTable();
+                                  });
+                              </script>
+                          </div>
+                      </div>
+                 
