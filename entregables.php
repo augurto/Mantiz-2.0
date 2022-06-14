fca6b60e8c64ebb8208f907e6cdf564e2833f3a1
Ego <augurto.17@gmail.com>
2022-06-14 05:08:47 -0500

    mostrando estado

--- entregables.php
+++ entregables.php
@@ -80,6 +80,7 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
     <!-- Fin de graficas -->
     <!-- Boton agregar proyecto -->
     <br>
+    <?php echo $estado_proyecto; ?>
     <?php if ($estado_proyecto==2) {
       echo "Proyecto inactivo";
     }else { ?>
