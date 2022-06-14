7f521ff1c2bd68501e8be65c222b28170592e9e0
Ego <augurto.17@gmail.com>
2022-06-13 20:10:15 -0500

    cmavbiadno de nombre

--- includes/parts/graficas_entregable_vacio.php
+++ includes/parts/graficas_entregable_vacio.php
@@ -44,7 +44,7 @@
                         <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Reporte Entregables</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"> Entregables</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tps;?></div>
                             </div>
                             <div class="col-auto">
@@ -63,7 +63,7 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Reporte Entregables vacios</div>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Entregables vacios</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tps2;?></div>
                     </div>
                     <div class="col-auto">
