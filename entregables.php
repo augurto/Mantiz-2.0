44c0b8e251c20b6902b3d8578ab59c7b2fbfff19
Ego <augurto.17@gmail.com>
2022-06-14 05:16:43 -0500

    ellasdsadxzc

--- entregables.php
+++ entregables.php
@@ -81,37 +81,38 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
     <!-- Boton agregar proyecto -->
 
     
-    <?php if ($estado_proyecto==2) {
-  
-    }elseif($estado_proyecto==1){ ?>
-            <br>
+    <?php if ($estado_proyecto==1) { ?>
+          <br>
    
-    <div class="container">
-        <div class="row justify-content-between">
-            <div class="col-6">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-entregable">
-                <span class="icon text-white-50">
-                            <i class="fas fa-plus"></i>
-                            
-                </span>Entregables
-                </button>
-
-              
-            </div>    
-            <div class="col-6">
-                <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modal-subir-archivos">
-                <span class="icon text-white-50">
-                            <i class="fas fa-plus"></i>
-                </span>Archivos
-                </button>
-
-                <?php include 'includes/modal/modal_entregable.php' ?>
-                <?php include 'includes/modal/modal-subir-archivos.php' ?>
-            <!-- Fin Boton agregar proyecto -->
-            </div>     
-        </div>    
-    </div>
+          <div class="container">
+              <div class="row justify-content-between">
+                  <div class="col-6">
+                      <!-- Button trigger modal -->
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-entregable">
+                      <span class="icon text-white-50">
+                                  <i class="fas fa-plus"></i>
+                                  
+                      </span>Entregables
+                      </button>
+      
+                    
+                  </div>    
+                  <div class="col-6">
+                      <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modal-subir-archivos">
+                      <span class="icon text-white-50">
+                                  <i class="fas fa-plus"></i>
+                      </span>Archivos
+                      </button>
+      
+                      <?php include 'includes/modal/modal_entregable.php' ?>
+                      <?php include 'includes/modal/modal-subir-archivos.php' ?>
+                  <!-- Fin Boton agregar proyecto -->
+                  </div>     
+              </div>    
+          </div>
+  
+        <?php }elseif($estado_proyecto==0){ echo'proyecto terminado'; ?>
+          
     <?php }?>
     <br>
     <!-- Contenido de la tabla -->
