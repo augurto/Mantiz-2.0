07082b228a6e0af16e001472464435cd5181c63d
Ego <augurto.17@gmail.com>
2022-06-22 18:20:05 -0500

    haciendo insert en actualizar entregables

--- includes/process/actualizar/actualizar_estado_entregable.php
+++ includes/process/actualizar/actualizar_estado_entregable.php
@@ -9,7 +9,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 $id=$_GET["id"];
 $id_p=$_GET["id_p"];
 $estado_entregable=$_GET["estado_entregable1"];
-
+$hoy = date("Y-m-d H:i:s"); 
 
 
 
@@ -23,6 +23,7 @@ echo "Connected successfully";
 $sql = "UPDATE  archivos set  a_estado_seguimiento = '".$estado_entregable."' where a_id = '".$id."'";
 /* $sql = "UPDATE  users set  tipo_user = '1' where id = '".$id_users."'"; */
 if (mysqli_query($conn, $sql)) {
+      $sql2 = "INSERT  auditoria (valor1, valor2, valor3, fecha_modificacion, tipo_dato, usuario, proyecto) VALUES ('$id','$id_p','$estado_entregable', '$hoy', 'Actualizacion de entregables','Ego Preuba', 'Proyecto prueba' ) ";
     echo"<script language ='JavaScript'>";
       echo "location='../../../entregables.php?id_p=$id_p'";
     echo "</script>";
