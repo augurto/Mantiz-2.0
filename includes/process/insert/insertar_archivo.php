ee34f12c43210e66edd3f599637e4689d406acd4
Ego <augurto.17@gmail.com>
2022-06-30 01:45:50 -0500

    enlazando a mantizb

--- includes/process/insert/insertar_archivo.php
+++ includes/process/insert/insertar_archivo.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$database = "u415020159_hermes";
-$username = "u415020159_hermes";
+$database = "u415020159_mantizb";
+$username = "u415020159_mantizb";
 $password = "Mantizb*#17";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
