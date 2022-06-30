189b4adc2a1ed287c9528128d9cd99525e828e4f
Ego <augurto.17@gmail.com>
2022-06-30 06:01:39 -0500

    llevando el usuario en el link

--- includes/parts/graficas_entregable_vacio.php
+++ includes/parts/graficas_entregable_vacio.php
@@ -103,7 +103,7 @@
 
             <!-- Pending Requests Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-            <a href="../../reporte_usuario.php">
+            <a href="../../reporte_usuario.php?usuario=<?php echo $usuario;?>">
               <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
