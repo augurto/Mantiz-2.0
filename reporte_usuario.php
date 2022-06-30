14692fd280a29ebc4f4a1f3f9fae53775c1bf028
Ego <augurto.17@gmail.com>
2022-06-30 07:34:53 -0500

    sin clase

--- reporte_usuario.php
+++ reporte_usuario.php
@@ -234,7 +234,7 @@ function toggleDataSeries(e){
   </div>
 </form>
 
-        <div class="h-auto d-inline-block" id="chartContainer" style="height: 370px;width: 100%;"></div>
+        <div  id="chartContainer" style="height: 370px;width: 100%;"></div>
   </div>
 </div>
     </div>
