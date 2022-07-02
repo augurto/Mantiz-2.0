fdbd05cac44943b74e26cda312ec33873c61e6f7
Ego <augurto.17@gmail.com>
2022-07-02 11:50:51 -0500

    faltaban script para las tablas

--- operaciones.php
+++ operaciones.php
@@ -17,9 +17,13 @@ require_once ("config/conexion.php");//Contiene funcion que conecta a la base de
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- incluyendo script para editar en tiempo realpat -->
+
+    
+    <!-- fin -->
     <!-- incluyendo bootstrap -->
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
     <script src="js/bootstrap.min.js"></script>
@@ -41,6 +45,11 @@ require_once ("config/conexion.php");//Contiene funcion que conecta a la base de
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">  
 
     <!-- fin datatable -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <title>Mantiz-Operaciones</title>
 </head>
 <body>
@@ -139,6 +148,9 @@ require_once ("config/conexion.php");//Contiene funcion que conecta a la base de
     <!-- código JS propìo-->    
     <script type="text/javascript" src="main.js"></script>   
 
+
+    <!-- Fin de Script para datatables -->
+    <script src="js/proyecto.js"></script>
     <!-- Fin de Script para datatables -->
 </body>
 </html>
