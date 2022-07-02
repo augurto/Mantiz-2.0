03339dec98b11383b33e7350dea2d50933d5157b
Ego <augurto.17@gmail.com>
2022-07-02 14:30:29 -0500

    agregando el where

--- includes/parts/tabla_ver_obras.php
+++ includes/parts/tabla_ver_obras.php
@@ -21,7 +21,7 @@
                                 </thead>
                         <?php foreach ($link->query('SELECT * from operaciones o inner join proyecto p on o.proyecto_operacion=p.codigo
                          inner join materiales m on o.nombre_operacion = m.id_mat
-                         inner join users u on o.usuario_maker_operacion = u.id') as $row){ // aca se hace la consulta e iterarla con each. ?> 
+                         inner join users u on o.usuario_maker_operacion = u.id WHERE proyecto_operacion ="'.$codigo_operacion.'"' ) as $row){ // aca se hace la consulta e iterarla con each. ?> 
                         <?php
                         $proyecto_operacion=$row['nombre_proyecto'];
                         $nombre_operacion=$row['nombre_material'];
