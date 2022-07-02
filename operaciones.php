2bd7e12cd7c59c88345bcce7bfe324783449d8f1
Ego <augurto.17@gmail.com>
2022-07-02 10:22:33 -0500

    agregasdno botones para alinnearlos

--- operaciones.php
+++ operaciones.php
@@ -50,6 +50,15 @@ require_once ("config/conexion.php");//Contiene funcion que conecta a la base de
     <div style="height:50px"></div>
     <!-- Inicio de Graficas -->
     <div class="container">
+    <div class="row">
+      <div class="col">
+        <button class="btn btn-danger form-control  btn-block" routerLink='/login'>Cancel</button></div>
+      <div class="col">
+        <button id="btnSubmit" class="btn btn-primary form-control btn-block" type="submit" (click)="onSubmit()">Submit</button></div>
+    </div>
+  </div>
+  
+    <div class="container">
         <div class="row justify-content-between">
             <div class="col-6">
                 <!-- Button trigger modal -->
