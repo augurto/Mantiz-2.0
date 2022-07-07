56e70ba03f43608c4076c11acea37a78332d7322
Ego <augurto.17@gmail.com>
2022-07-07 16:34:18 -0500

    mostrando todos los proyectos

--- includes/parts/rendimiento/rendimiento0.php
+++ includes/parts/rendimiento/rendimiento0.php
@@ -1,18 +1,19 @@
  <!--     inicio de contenido  -->
- <?php foreach ($link->query('SELECT * from archivos where usuario_asignado_archivo ="'.$id_username.'"') as $row2){ // aca puedes hacer la consulta e iterarla con each.
+ <?php foreach ($link->query('SELECT * from proyectos ') as $row2){ // aca puedes hacer la consulta e iterarla con each.
                             $contador=1; 
-                            $id2=$row2['a_id'];
-                            $documento=$row2['documento'];
+                            $codigo=$row2['codigo'];
+                            $nombre_proyecto=$row2['nombre_proyecto'];
+                            $extension=$row2['extension'];
                           /*  $cod_proyecto=$row['a_codigo_proyecto']; */ ?> 
                               
                                     <div class="accordion accordion-flush" id="accordionFlushExample">
                                         <div class="accordion-item">
                                           <h2 class="accordion-header" id="flush-headingOne">
-                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $id2; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
-                                              <?php echo $documento ; ?>
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $codigo; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
+                                              <?php echo $extension.' - '.$nombre_proyecto ; ?>
                                             </button>
                                           </h2>
-                                          <div id="flush-collapse<?php echo $id2; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                          <div id="flush-collapse<?php echo $codigo; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                             <div class="accordion-body"> 
                                             <?php include 'rendimiento1.php'; ?>
 
