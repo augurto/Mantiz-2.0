673215c2e739e7ae88c794196974193ee68316a5
Ego <augurto.17@gmail.com>
2022-07-07 06:46:02 -0500

    con usuario

--- includes/parts/rendimiento.php
+++ includes/parts/rendimiento.php
@@ -9,11 +9,11 @@
             <div class="accordion accordion-flush" id="accordionFlushExample">
                 <div class="accordion-item">
                   <h2 class="accordion-header" id="flush-headingOne">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $count++; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $username; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
                       <?php echo $username ; ?>
                     </button>
                   </h2>
-                  <div id="flush-collapse<?php echo $count++; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                  <div id="flush-collapse<?php echo $username; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                     <div class="accordion-body"> <?php echo $cod_proyecto ; ?></div>
                   </div>
                   </div>
