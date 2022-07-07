4593b78b85fb36bc8912e5f3c557de103d314057
Ego <augurto.17@gmail.com>
2022-07-07 06:10:06 -0500

    probando el for

--- includes/parts/rendimiento.php
+++ includes/parts/rendimiento.php
@@ -30,3 +30,36 @@
     </div>
   </div>
 </div>
+
+<!-- inicio de la tabla -->
+<table class="table">
+  <!-- <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead> -->
+  <?php foreach ($link->query('SELECT * from users ') as $row){ // aca puedes hacer la consulta e iterarla con each.
+    $username=$row['username']; ?> 
+        <tbody>
+          <tr>
+            <td>
+             <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                      <?php echo $username ; ?>
+                    </button>
+                  </h2>
+                  <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                  </div>
+              </div>
+            </td>
+          </tr>
+         
+        </tbody>
+  <?php } ?> 
+</table>
