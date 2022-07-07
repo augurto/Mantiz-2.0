c8c0c215f49dd5cae426062b484bfdfed95e7846
Ego <augurto.17@gmail.com>
2022-07-07 07:05:50 -0500

    xzc

--- includes/parts/rendimiento.php
+++ includes/parts/rendimiento.php
@@ -1,20 +1,20 @@
 
 <!-- inicio de la tabla -->
 
-  <?php foreach ($link->query('SELECT * from users u inner join archivos a on u.id=a.usuario_asignado_archivo group by username') as $row){ // aca puedes hacer la consulta e iterarla con each.
+  <?php foreach ($link->query('SELECT * from users group by username') as $row){ // aca puedes hacer la consulta e iterarla con each.
     $contador=1; 
     $username=$row['username'];
-    $cod_proyecto=$row['a_codigo_proyecto']; ?> 
+   /*  $cod_proyecto=$row['a_codigo_proyecto']; */ ?> 
        
             <div class="accordion accordion-flush" id="accordionFlushExample">
                 <div class="accordion-item">
                   <h2 class="accordion-header" id="flush-headingOne">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $username; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
-                      <?php echo $username.'AGREGAR' ; ?>
+                      <?php echo $username ; ?>
                     </button>
                   </h2>
                   <div id="flush-collapse<?php echo $username; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body"> <?php echo $cod_proyecto ; ?></div>
+                    <div class="accordion-body"> <?php echo 'descripciomn' ; ?></div>
                   </div>
                   </div>
             </div>
