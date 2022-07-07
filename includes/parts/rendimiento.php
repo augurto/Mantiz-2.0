c11f456aad328f08b60c025ff8c25d7b95b3d09f
Ego <augurto.17@gmail.com>
2022-07-07 07:15:44 -0500

    acordeon dentro de acordeon XD

--- includes/parts/rendimiento.php
+++ includes/parts/rendimiento.php
@@ -4,6 +4,7 @@
   <?php foreach ($link->query('SELECT * from users group by username') as $row){ // aca puedes hacer la consulta e iterarla con each.
     $contador=1; 
     $username=$row['username'];
+    $id_username=$row['id'];
    /*  $cod_proyecto=$row['a_codigo_proyecto']; */ ?> 
        
             <div class="accordion accordion-flush" id="accordionFlushExample">
@@ -14,7 +15,31 @@
                     </button>
                   </h2>
                   <div id="flush-collapse<?php echo $username; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body"> <?php echo 'descripciomn' ; ?></div>
+                    <div class="accordion-body">
+                        <?php foreach ($link->query('SELECT * from archivos') as $row2){ // aca puedes hacer la consulta e iterarla con each.
+                            $contador=1; 
+                            $id2=$row2['a_id'];
+                            $documento=$row2['documento'];
+                          /*  $cod_proyecto=$row['a_codigo_proyecto']; */ ?> 
+                              
+                                    <div class="accordion accordion-flush" id="accordionFlushExample">
+                                        <div class="accordion-item">
+                                          <h2 class="accordion-header" id="flush-headingOne">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $id2; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
+                                              <?php echo $username ; ?>
+                                            </button>
+                                          </h2>
+                                          <div id="flush-collapse<?php echo $id2; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                            <div class="accordion-body"> <?php echo 'documento' ; ?></div>
+                                          </div>
+                                          </div>
+                                    </div>
+                                
+                          <?php } ?> 
+                      
+                  
+                  
+                    </div>
                   </div>
                   </div>
             </div>
