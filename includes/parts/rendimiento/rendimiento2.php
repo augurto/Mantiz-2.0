2f7f5a5dff0449207f35f6a687f00ca3f4a6dd7f
Ego <augurto.17@gmail.com>
2022-07-07 18:01:11 -0500

    error de nombre en la as variables

--- includes/parts/rendimiento/rendimiento2.php
+++ includes/parts/rendimiento/rendimiento2.php
@@ -2,16 +2,16 @@
  <?php foreach ($link->query('SELECT a.id_seg, a.documento, a.a_estado_seguimiento,a.nombre FROM archivos a where id_seg ="'.$id_entregable.'" ') as $row3){ 
 
        $codigo=$row3['codigo'];
-       $nombre_entregable=$row3['documento'];
-       $espacio=trim($nombre_entregable);
-       $usuario_asignado_entregable=$row3['usuario_asignado_entregable'];                  
+       $nombre_documento=$row3['documento'];
+       $espacio=trim($nombre_documento);
+       $usuario_asignado_archivo=$row3['usuario_asignado_entregable'];                  
                                                         ?> 
                                                               
                                                                     <div class="accordion accordion-flush" id="accordionFlushExample">
                                                                         <div class="accordion-item" style="border: 1px solid #FF252C;">
                                                                           <h2 class="accordion-header" id="flush-headingOne">
                                                                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $espacio; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
-                                                                              <?php echo $nombre_entregable ; ?>
+                                                                              <?php echo $nombre_documento ; ?>
                                                                             </button>
                                                                           </h2>
                                                                           <div id="flush-collapse<?php echo $espacio; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
