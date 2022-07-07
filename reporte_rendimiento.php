a3c65fa56a75b2b894dc2da48e7deb61e72ba2f2
Ego <augurto.17@gmail.com>
2022-07-07 05:35:32 -0500

    agregando link

--- reporte_rendimiento.php
+++ reporte_rendimiento.php
@@ -43,7 +43,8 @@ require_once ("config/conexion.php");//Contiene funcion que conecta a la base de
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <!-- CSS personalizado --> 
     <link rel="stylesheet" href="main.css">  
-      
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <!--datables CSS básico-->
     <link rel="stylesheet" type="text/css" href="datatables/datatables.min.css"/>
     <!--datables estilo bootstrap 4 CSS-->  
