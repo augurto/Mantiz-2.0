acbaec70633696e3dc51cc93acb3623865a3d61a
Ego <augurto.17@gmail.com>
2022-07-07 17:51:56 -0500

    de nuevo

--- includes/parts/rendimiento/rendimiento2.php
+++ includes/parts/rendimiento/rendimiento2.php
@@ -3,17 +3,18 @@
 
        $codigo=$row3['codigo'];
        $nombre_entregable=$row3['documento'];
+       $espacio=trim($nombre_entregable);
        $usuario_asignado_entregable=$row3['usuario_asignado_entregable'];                  
                                                         ?> 
                                                               
                                                                     <div class="accordion accordion-flush" id="accordionFlushExample">
                                                                         <div class="accordion-item" style="border: 1px solid #FF252C;">
                                                                           <h2 class="accordion-header" id="flush-headingOne">
-                                                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $nombre_entregable; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
+                                                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $espacio; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
                                                                               <?php echo $nombre_entregable ; ?>
                                                                             </button>
                                                                           </h2>
-                                                                          <div id="flush-collapse<?php echo $nombre_entregable; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                                                          <div id="flush-collapse<?php echo $espacio; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                                                             <div class="accordion-body"> 
                                                                           
                                                                             <?php include 'rendimiento2.php'; ?>
