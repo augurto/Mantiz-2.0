3b19e819bd0b046ae84c4ed796ee4c60491923f5
Ego <augurto.17@gmail.com>
2022-07-07 06:49:41 -0500

    XCZXC

--- includes/parts/rendimiento.php
+++ includes/parts/rendimiento.php
@@ -10,7 +10,7 @@
                 <div class="accordion-item">
                   <h2 class="accordion-header" id="flush-headingOne">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $username; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
-                      <?php echo $username ; ?>
+                      <?php echo $username.'AGREGAR' ; ?>
                     </button>
                   </h2>
                   <div id="flush-collapse<?php echo $username; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
