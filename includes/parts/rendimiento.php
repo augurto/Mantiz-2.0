7ab1b80c48f05902737f52ca82376881529586a1
Ego <augurto.17@gmail.com>
2022-07-08 09:02:12 -0500

    abora si

--- includes/parts/rendimiento.php
+++ includes/parts/rendimiento.php
@@ -48,23 +48,21 @@
                                                                                                                       ?> 
                                                                                                                             
                                                                    
-                                                                              <?php echo $nombre_entregable ; ?>
-                                                                              <br>
                                                                               
                                                                               <div class="w-75 p-3" style="background-color: #eee;">
-                                                                              <div class="accordion" id="accordionExample">
+                                                                              <div class="accordion" id="accordionExampleq">
                                                                               <div class="accordion-item">
-                                                                                <h2 class="accordion-header" id="headingOne">
-                                                                                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                                                                    Accordion Item #1
+                                                                                <h2 class="accordion-header" id="headingTwo">
+                                                                                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                                                  <?php echo $nombre_entregable ; ?>
                                                                                   </button>
                                                                                 </h2>
-                                                                                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExampleq">
                                                                                   <div class="accordion-body">
-                                                                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                                                                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                                                                                   </div>
                                                                                 </div>
-                                                                                </div>
+                                                                              </div>
                                                                                 </div>
                                                                                 </div>
                                                                               <br>
