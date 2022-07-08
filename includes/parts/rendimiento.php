f70392f46328fe95b9caa9d142842d7b9599dac0
Ego <augurto.17@gmail.com>
2022-07-08 08:31:51 -0500

    alinenado a la drecha

--- includes/parts/rendimiento.php
+++ includes/parts/rendimiento.php
@@ -50,7 +50,9 @@
                                                                    
                                                                               <?php echo $nombre_entregable ; ?>
                                                                               <br>
+                                                                              <div class="position-absolute top-0 start-100 translate-middle">
                                                                               <div class="w-75 p-3" style="background-color: #eee;">Width 75%</div>
+                                                                              </div>
                                                                               <br>
                                                                           
                                                           <?php } ?> 
