fec355e1a263b63a6c7258b5139efa01232139d8
Ego <augurto.17@gmail.com>
2022-07-08 07:03:43 -0500

    creo

--- includes/parts/rendimiento/rendimiento0.php
+++ includes/parts/rendimiento/rendimiento0.php
@@ -1,5 +1,5 @@
  <!--     inicio de contenido  -->
- <?php foreach ($link->query('SELECT p.codigo,p.extension,p.nombre_proyecto,e.codigo_proyecto, e.nombre, e.usuario_asignado_entregable 
+ <?php foreach ($link->query('SELECT p.codigo,p.extencion,p.nombre_proyecto,e.codigo_proyecto, e.nombre, e.usuario_asignado_entregable 
  FROM entregables e INNER JOIN proyecto p on e.codigo_proyecto=p.id
  WHERE usuario_asignado_entregable ="'.$id_username.'" GROUP by p.nombre_proyecto') as $row2){ // aca puedes hacer la consulta e iterarla con each.
                             
