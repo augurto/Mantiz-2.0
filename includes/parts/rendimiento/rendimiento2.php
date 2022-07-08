96f458d6c4d77e9f15fc7b708a2b2f601183755e
Ego <augurto.17@gmail.com>
2022-07-08 05:06:30 -0500

    mostrando estado

--- includes/parts/rendimiento/rendimiento2.php
+++ includes/parts/rendimiento/rendimiento2.php
@@ -1,24 +1,27 @@
  <!--     inicio de contenido  -->
- <?php foreach ($link->query('SELECT a.id_seg, a.documento, a.a_estado_seguimiento FROM archivos a ') as $row4){ 
+ <?php foreach ($link->query('SELECT a.id_seg,a.a_codigo_proyecto,a.a_id , a.documento, a.a_estado_seguimiento FROM archivos a ') as $row4){ 
 
-       $codigo=$row4['id_seg'];
-       $nombre_documento=$row4['documento'];
-       $espacio=trim($nombre_documento);
+        $id_seg_archivos=$row4['id_seg'];
+        $a_codigo_proyecto_archivos=$row4['a_codigo_proyecto'];
+        $a_id_archivos=$row4['a_id'];
+        $a_estado_seguimiento_archivos=$row4['a_estado_seguimiento'];
+        $nombre_documento=$row4['documento'];
+        $espacio=trim($nombre_documento);
                        
-                                                        ?> 
+?> 
                                                               
                                                                     <div class="accordion accordion-flush" id="accordionFlushExample">
                                                                         <div class="accordion-item" style="border: 1px solid #FF252C;">
                                                                           <h2 class="accordion-header" id="flush-headingOne">
-                                                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $espacio; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
+                                                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo 'parte4'.$a_id_archivos; ?>" aria-expanded="false" aria-controls="flush-collapseOne">
                                                                               <?php echo $nombre_documento ; ?>
                                                                             </button>
                                                                           </h2>
-                                                                          <div id="flush-collapse<?php echo $espacio; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                                                          <div id="flush-collapse<?php echo 'parte4'.$a_id_archivos; ?>" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                                                             <div class="accordion-body"> 
                                                                           
-                                                                         
-                                                                            prueba del ultimo if
+                                                                            <?php echo $a_estado_seguimiento_archivos ; ?>
+                                                                           
                                                                             </div>
                                                                           </div>
                                                                           </div>
