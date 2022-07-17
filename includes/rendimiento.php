30027d3877a6f6b67d24bb1b086ad37e8bc043a2
Ego <augurto.17@gmail.com>
2022-07-17 11:41:06 -0500

    zxczxc

--- includes/rendimiento.php
+++ includes/rendimiento.php
@@ -4,7 +4,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
-                <button class="accordion2">Section 2</button>
+                <button class="accordion2">Section 2ewe</button>
                 <div class="panel2">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
@@ -24,6 +24,7 @@
                 <button class="accordion2">Section 3</button>
                 <div class="panel2">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                
                 </div>
 
                 <script>
