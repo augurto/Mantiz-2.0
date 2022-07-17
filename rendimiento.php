d8b70667a9497c6b5da7638b3017b48e60c9dd93
Ego <augurto.17@gmail.com>
2022-07-17 11:48:06 -0500

    zxc1

--- rendimiento.php
+++ rendimiento.php
@@ -81,9 +81,10 @@
                 <div class="panel" style="height: 110% !important;">
                         
                             <?php include 'includes/rendimiento.php';?>
+                            <br>
                  
                 </div>
-
+                <br>
         <button class="accordion">Section 2</button>
                 <div class="panel">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
