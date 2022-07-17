aba7200c85f5ae73aa34c4038d8681d2864c9b69
Ego <augurto.17@gmail.com>
2022-07-17 11:30:41 -0500

    accordion2

--- includes/rendimiento.php
+++ includes/rendimiento.php
@@ -1,15 +1,15 @@
 
-        <button class="accordion">Section 2.5</button>
+        <button class="accordion2">Section 2.5</button>
                 <div class="panel2">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
-        <button class="accordion">Section 2</button>
+        <button class="accordion2">Section 2</button>
                 <div class="panel2">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
-        <button class="accordion">Section 3</button>
+        <button class="accordion2">Section 3</button>
                 <div class="panel2">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
