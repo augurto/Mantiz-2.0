e940f1ab8d0ef5d38f236494de07294a4697da6f
Ego <augurto.17@gmail.com>
2022-07-20 08:44:49 -0500

    opacity:0.2;

--- includes/parts/tabla-inicio.php
+++ includes/parts/tabla-inicio.php
@@ -46,7 +46,7 @@
                              <tr style="background-color: red !important;">
 
                              <?php  } else{?>
-                             <tr style="background-color: blue !important;">
+                             <tr style="background-color: blue !important; opacity:0.5;">
                              <?php }?>   
                              <td><?php echo $count++; ?></td>
                              
