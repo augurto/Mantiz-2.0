5282232f6f9d4f4b0c363ed61c014ab42741f6a7
Ego <augurto.17@gmail.com>
2022-07-20 07:12:05 -0500

    arregladno el contexto

--- includes/parts/graficas.php
+++ includes/parts/graficas.php
@@ -64,7 +64,7 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Proyectos Terminados</div>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Proyectos   Terminados </div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tes;?></div>
                     </div>
                     <div class="col-auto">
